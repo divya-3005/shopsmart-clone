1eae8c70aa49941a62976a3977f56f1864a9e571
Divya Singh <divyasorrot3005@gmail.com>
2026-04-20 22:00:43 +0530

    Fix: Add port 22 (SSH) to security group for management server access

--- terraform/main.tf
+++ terraform/main.tf
@@ -158,6 +158,13 @@ resource "aws_security_group" "ecs_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
